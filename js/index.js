7920e77bb17919e2723344d69d4815b02af96f13
glecours <glecours0@gmail.com>
2017-08-08 10:58:21 -0400

    adding files

---
+++ js/index.js
@@ -0,0 +1,65 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+        
+    onDeviceReady: function() {
+		 app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        
+		window.analytics.startTrackerWithId('UA-76451657-2');
+		window.analytics.trackView('First load');
+    }
+	
+};
+app.initialize();
+
+
+document.addEventListener('deviceready', function () {
+  
+  var notificationOpenedCallback = function(jsonData) {
+    console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+  };
+
+  window.plugins.OneSignal
+    .startInit("f4bdeb26-5602-4663-9fc4-dbe8a617810b", "875029484681")
+    .handleNotificationOpened(notificationOpenedCallback)
+    .endInit();
+}, false);
+
+window.plugins.OneSignal.getIds(function(ids) {
+  console.log('getIds: ' + JSON.stringify(ids));
+  alert("userId = " + ids.userId + ", pushToken = " + ids.pushToken);
+});
